8fe688db1e453dce7a0bfc82d80ba01a5294eedd
Zakharov Svyatoslav <kraldraav@gmail.com>
2024-12-27 11:07:45 +0300

    Init commit

---
+++ AkkaBootcamp.WinTail/ConsoleReaderActor.cs
@@ -0,0 +1,42 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaBootcamp.WinTail;
+
+/// <summary>
+/// Actor responsible for reading FROM the console. 
+/// Also responsible for calling <see cref="ActorSystem.Terminate"/>.
+/// </summary>
+public class ConsoleReaderActor : UntypedActor
+{
+    public const string ExitCommand = "exit";
+    private readonly IActorRef _consoleWriterActor;
+
+    public ConsoleReaderActor(IActorRef consoleWriterActor)
+    {
+        _consoleWriterActor = consoleWriterActor;
+    }
+
+    protected override void OnReceive(object message)
+    {
+        var read = Console.ReadLine();
+        if(!string.IsNullOrEmpty(read) 
+            && string.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+        {
+            // shut down the system (acquire handle to system via
+            // this actors context)
+            Context.System.Terminate();
+            return;
+        }
+
+        // send input to the console writer to process and print
+        // YOU NEED TO FILL IN HERE
+
+        // continue reading messages from the console
+        // YOU NEED TO FILL IN HERE
+    }
+}
---
+++ AkkaBootcamp.WinTail/ConsoleWriterActor.cs
@@ -0,0 +1,38 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaBootcamp.WinTail;
+
+/// <summary>
+/// Actor responsible for serializing message writes to the console.
+/// (write one message at a time, champ :)
+/// </summary>
+public class ConsoleWriterActor : UntypedActor
+{
+    protected override void OnReceive(object message)
+    {
+        var msg = message as string;
+
+        if (string.IsNullOrEmpty(msg))
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Please provide an input.\n");
+            Console.ResetColor();
+            return;
+        }
+
+        var even = msg.Length % 2 == 0;
+        var color = even ? ConsoleColor.Red : ConsoleColor.Green;
+        var alert = even
+            ? "Your string had an even # of characters.\n"
+            : "Your string had an odd  # of characters.\n";
+
+        Console.ForegroundColor = color;
+        Console.WriteLine(alert);
+        Console.ResetColor();
+    }
+}
