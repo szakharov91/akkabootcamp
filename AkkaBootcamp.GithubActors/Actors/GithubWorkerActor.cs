755b9a2d485bd7d8896fdd49388fd62d7bae6bd3
Zakharov Svyatoslav <kraldraav@gmail.com>
2025-01-03 13:46:21 +0300

    Lesson 3.4: How to perform work asynchronously inside your actors using PipeTo

--- AkkaBootcamp.GithubActors/Actors/GithubWorkerActor.cs
+++ AkkaBootcamp.GithubActors/Actors/GithubWorkerActor.cs
@@ -69,20 +69,16 @@ namespace AkkaBootcamp.GithubActors.Actors
             {
                 // ReSharper disable once PossibleNullReferenceException (we know from the previous IS statement that this is not null)
                 var starrer = (query.Query as QueryStarrer).Login;
-                try
-                {
-                    var getStarrer = _gitHubClient.Activity.Starring.GetAllForUser(starrer);
 
-                    //ewww
-                    getStarrer.Wait();
-                    var starredRepos = getStarrer.Result;
-                    Sender.Tell(new StarredReposForUser(starrer, starredRepos));
-                }
-                catch (Exception)
-                {
-                    //operation failed - let the parent know
-                    Sender.Tell(query.NextTry());
-                }
+                var sender = Sender;
+                _gitHubClient.Activity.Starring.GetAllForUser(starrer)
+                    .ContinueWith<object>(tr =>
+                    {
+                        if (tr.IsFaulted || tr.IsCanceled)
+                            return query.NextTry();
+
+                        return new StarredReposForUser(starrer, tr.Result);
+                    }).PipeTo(sender);
             });
 
             //query all starrers for a repository
@@ -90,20 +86,17 @@ namespace AkkaBootcamp.GithubActors.Actors
             {
                 // ReSharper disable once PossibleNullReferenceException (we know from the previous IS statement that this is not null)
                 var starrers = (query.Query as QueryStarrers).Key;
-                try
-                {
-                    var getStars = _gitHubClient.Activity.Starring.GetAllStargazers(starrers.Owner, starrers.Repo);
 
-                    //ewww
-                    getStars.Wait();
-                    var stars = getStars.Result;
-                    Sender.Tell(stars.ToArray());
-                }
-                catch (Exception)
-                {
-                    //operation failed - let the parent know
-                    Sender.Tell(query.NextTry());
-                }
+                var sender = Sender;
+
+                _gitHubClient.Activity.Starring.GetAllStargazers(starrers.Owner, starrers.Repo)
+                    .ContinueWith<object>(tr =>
+                    {
+                        if (tr.IsFaulted || tr.IsCanceled)
+                            return query.NextTry();
+
+                        return tr.Result.ToArray();
+                    }).PipeTo(sender);
             });
         }
     }
